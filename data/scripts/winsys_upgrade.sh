38f81ce4fe8f775f69d618c89fe4377d731adf9e
Sunmin Lee <sunm.lee@samsung.com>
2017-10-17 16:57:29 +0900

    Remove not supported update script
    RW update script for Tizen 2.4 (to 3.0) is not necessary in Tizen 4.0.
    (Tizen 4.0 does not support upgrade from Tizen 2.4)
    Remove the codes for Tizen 2.4 in terms of clearing the redundancy.
    
    Change-Id: I6cd76071851f2e27d233c740d067883ef8f72c39

--- data/scripts/winsys_upgrade.sh
+++ data/scripts/winsys_upgrade.sh
@@ -3,17 +3,7 @@
 source /usr/share/upgrade/rw-update-macro.inc
 get_version_info
 
-if [ $OLD_VER == "2.4" ]; then
-#Remove directories and files installed from the old version of platform
-rm -rf /var/xkb
-rm -rf /var/log/Xorg*.log*
-rm -rf /opt/etc/dump.d/module.d/winsys_log_dump.sh
-rm -rf /opt/home/app/.e
-
-#Create Xkb cache directory
-mkdir -p /var/lib/xkb
-
-elif [ $OLD_VER = "3.0" ]; then
+if [ $OLD_VER = "3.0" ]; then
 rm -rf /var/lib/xkb
 
 mkdir -p /var/lib/xkb
